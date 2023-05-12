ae25aa4a20c91d09360e07904c7f076a881d28a5
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2023-05-12 11:31:39 +0300

    add expanation file

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import cv2
 TILE_SIZE = 41
 
 
-def rotate_tile_to_original(tile_rotated: np.ndarray, rotation_direction: str) -> np.ndarray:
+def rotate_tile_to_original_angle(tile_rotated: np.ndarray, rotation_direction: str) -> np.ndarray:
     """
     rotate tile to original form
     :param tile_rotated: tile rotated clockwise or counterclockwise
@@ -16,7 +16,12 @@ def rotate_tile_to_original(tile_rotated: np.ndarray, rotation_direction: str) -
                       cv2.ROTATE_90_CLOCKWISE) if rotation_direction == "clockwise" else cv2.rotate(
         tile_rotated, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-def find_tile_and_place_in_descrambled(tile : tuple):
+
+def find_tile_and_place_in_descrambled(tile: tuple):
+    """
+    find the tile in the scrambled image, rotate it back to its original angle
+    :param tile: tile in the scrambled image
+    """
     # divide tile's properties to source coordinates, destination coordinates and rotation
     source, destination, rotation = tile[:2], tile[2:4], tile[4]
     # find tiles coordinates in scrambled image using destination coordinates
@@ -25,12 +30,12 @@ def find_tile_and_place_in_descrambled(tile : tuple):
     tile_in_scrambled = scrambled_image[y_in_scrambled:y_in_scrambled + TILE_SIZE,
                         x_in_scrambled:x_in_scrambled + TILE_SIZE]
     # rotate tile back to its original angle
-    tile_descrambled = rotate_tile_to_original(tile_rotated=tile_in_scrambled, rotation_direction=rotation)
+    tile_descrambled = rotate_tile_to_original_angle(tile_rotated=tile_in_scrambled, rotation_direction=rotation)
 
     # place descrambled tile in descrambled image
     x_in_descrambled, y_in_descrambled = source[0] * TILE_SIZE, source[1] * TILE_SIZE
     descrambled_image[y_in_descrambled:y_in_descrambled + TILE_SIZE,
-    x_in_descrambled:x_in_descrambled + TILE_SIZE] = tile_descrambled
+                        x_in_descrambled:x_in_descrambled + TILE_SIZE] = tile_descrambled
 
 
 # connecting to db
@@ -53,5 +58,3 @@ for tile in all_tiles:
     find_tile_and_place_in_descrambled(tile=tile)
 
 cv2.imwrite("Descrambled Image.jpg", descrambled_image)
-
-
